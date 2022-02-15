8f759051a4f3e7e5a5453a43ff0c4584520ca04b
Victor Farias <victor.aefarias@gmail.com>
2022-02-15 16:34:30 -0300

    moving flask app

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 import prify.config as config
 from prify import apis
 
+app = Flask(__name__)
+
 def init():    
     Path(config.anonymized_folder).mkdir(parents=True, exist_ok=True)
     Path(config.uploaded_folder).mkdir(parents=True, exist_ok=True)
         
 if __name__ == "__main__":
     init()
-    app = Flask(__name__)
+    
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
     apis.api.init_app(app)
