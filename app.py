e4fbeb1c9d0d4708cda62045d82bd0c1e1d11684
unknown <aglailsonsantiago@gmail.com>
2022-08-16 14:10:35 -0300

    api implementation v1

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
+from flask import Flask
+from flask_restful import Api
 from distutils.command.config import config
 from pathlib import Path
-
-from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
-
 import prify.config as config
-from prify import apis
+from prify.synthetics_api import SynthetizeCorrelated, SynthetizeCorrelatedDataset, SynthetizeCorrelatedStatus
 
-def init():    
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+api = Api(app)
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
+api.add_resource(SynthetizeCorrelated, '/synthetics/correlated')
+api.add_resource(SynthetizeCorrelatedStatus, '/synthetics/correlated/status/<string:file_id>')
+api.add_resource(SynthetizeCorrelatedDataset, '/synthetics/correlated/dataset/<string:file_id>')
+
+if __name__ == '__main__':
     Path(config.anonymized_folder).mkdir(parents=True, exist_ok=True)
     Path(config.uploaded_folder).mkdir(parents=True, exist_ok=True)
 
-app = Flask(__name__)
-init()
-app.wsgi_app = ProxyFix(app.wsgi_app)
-apis.api.init_app(app)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
 
-
-if __name__ == "__main__":
+    from sql_alchemy import banco
+    banco.init_app(app)
+    
     app.run(debug=True)
--- prify/synthetics_api.py
+++ prify/synthetics_api.py
@@ -5,11 +5,33 @@ import prify.config as config
 from DataSynthesizer.DataDescriber import DataDescriber
 from DataSynthesizer.DataGenerator import DataGenerator
 from flask import jsonify, request, send_file
-from flask_restx import Namespace, Resource, fields
+from flask_restful import Resource
+from sql_alchemy import banco
 
-api = Namespace("synthetics")
+class FileModel(banco.Model):
+    __tablename__ = 'files'
+    file_id = banco.Column(banco.String, primary_key=True)
+    path = banco.Column(banco.String)
 
-@api.route("/correlated")
+    def __init__(self, file_id, path):
+        self.file_id = file_id
+        self.path = path
+    
+    def json(self):
+        return {
+            'file_id': self.file_id,
+            'path': self.path
+        }
+
+    def save_file(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    @classmethod
+    def find_file(cls, file_id):
+        file = cls.query.filter_by(file_id=file_id).first()
+        return file
+
 class SynthetizeCorrelated(Resource):
     
     def post(self):
@@ -20,9 +42,14 @@ class SynthetizeCorrelated(Resource):
             return jsonify({'status': 'error', 'message': 'The file type must be CSV.'})
 
         uploaded_file.save(os.path.join(config.uploaded_folder, file_name+'.csv')) # we should delete this data after the process
-        return self.anonimyze_file(file_name=file_name)
+        
+        file = FileModel(file_name, config.uploaded_folder)
+        file.save_file()
+        
+        return file.json(), 202
+        #return self.anonimyze_file(file_name=file_name)
 
-
+    '''
     def anonimyze_file(self, file_name):
         data_path = os.path.join(config.anonymized_folder, f'{file_name}.csv')
         describe_path = os.path.join(config.anonymized_folder, f'{file_name}_desc.json')
@@ -44,37 +71,15 @@ class SynthetizeCorrelated(Resource):
         generator.save_synthetic_data(data_path)
 
         return send_file(data_path, as_attachment=True)
+    '''
 
-@api.route("/independent")
-class SynthetizeIndependent(Resource):
-    
-    def post(self):
-        file_name = str(uuid.uuid4())
-        uploaded_file = request.files['file']
+class SynthetizeCorrelatedStatus(Resource):
+    def get(self, file_id):
 
-        if uploaded_file.mimetype != 'text/csv':
-            return jsonify({'status': 'error', 'message': 'The file type must be CSV.'})
+        file = FileModel.find_file(file_id)
 
-        uploaded_file.save(os.path.join(config.uploaded_folder, file_name+'.csv')) # we should delete this data after the process
-        return self.anonimyze_file(file_name=file_name)
+        return file.json(), 200
 
-
-    def anonimyze_file(self, file_name):        
-        data_path = os.path.join(config.anonymized_folder, f'{file_name}.csv')
-        describe_path = os.path.join(config.anonymized_folder, f'{file_name}_desc.json')
-
-        output_size = request.args.get('sz', default = 1000, type = int)
-        epsilon = request.args.get('eps', default = 1, type = int)
-        threshold_value = request.args.get('threshold_value', default = 20, type = int)
-
-        print(f'Running w/ parametes: size={output_size} eps={epsilon} thr={threshold_value}')
-
-        describer = DataDescriber(category_threshold=threshold_value)
-        describer.describe_dataset_in_independent_attribute_mode(dataset_file=os.path.join(config.uploaded_folder, f'{file_name}.csv'))
-        describer.save_dataset_description_to_file(describe_path)
-        generator = DataGenerator()
-        # generator.generate_dataset_in_correlated_attribute_mode(output_size, describe_path) # resolve this params
-        generator.generate_dataset_in_independent_mode(output_size, describe_path)
-        generator.save_synthetic_data(data_path)
-
-        return send_file(data_path, as_attachment=True)
+class SynthetizeCorrelatedDataset(Resource):
+    def get():
+        return 500
