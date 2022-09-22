534cd29739c41aaa32141502bcd7637af6ab50e9
unknown <aglailsonsantiago@gmail.com>
2022-09-22 13:06:45 -0300

    Implementation of the asynchronous solution with MongoDB, RabbitMQ e Celery

--- prify/synthetics_api.py
+++ prify/synthetics_api.py
@@ -1,85 +1,62 @@
 import os
 import uuid
+import pymongo
+import boto3
 
 import prify.config as config
-from DataSynthesizer.DataDescriber import DataDescriber
-from DataSynthesizer.DataGenerator import DataGenerator
 from flask import jsonify, request, send_file
-from flask_restful import Resource
-from sql_alchemy import banco
+from flask_restx import Namespace, Resource
+from celery import Celery
+from celery.result import AsyncResult
 
-class FileModel(banco.Model):
-    __tablename__ = 'files'
-    file_id = banco.Column(banco.String, primary_key=True)
-    path = banco.Column(banco.String)
+api = Namespace("synthetics")
 
-    def __init__(self, file_id, path):
-        self.file_id = file_id
-        self.path = path
-    
-    def json(self):
-        return {
-            'file_id': self.file_id,
-            'path': self.path
-        }
-
-    def save_file(self):
-        banco.session.add(self)
-        banco.session.commit()
-
-    @classmethod
-    def find_file(cls, file_id):
-        file = cls.query.filter_by(file_id=file_id).first()
-        return file
-
+simple_app = Celery('simple_worker', broker='amqp://localhost',
+                                    backend='mongodb://localhost:27017/mydb')
+@api.route("/correlated")
 class SynthetizeCorrelated(Resource):
     
     def post(self):
+        
+
         file_name = str(uuid.uuid4())
+
         uploaded_file = request.files['file']
 
         if uploaded_file.mimetype != 'text/csv':
             return jsonify({'status': 'error', 'message': 'The file type must be CSV.'})
 
+        
+        api.logger.info("Invoking Method ")
         uploaded_file.save(os.path.join(config.uploaded_folder, file_name+'.csv')) # we should delete this data after the process
-        
-        file = FileModel(file_name, config.uploaded_folder)
-        file.save_file()
-        
-        return file.json(), 202
+        r = simple_app.send_task('tasks.anonimyze_file', kwargs={'file_name':file_name})
+        api.logger.info(r.backend)
+        return r.id, 202
         #return self.anonimyze_file(file_name=file_name)
 
-    '''
-    def anonimyze_file(self, file_name):
-        data_path = os.path.join(config.anonymized_folder, f'{file_name}.csv')
-        describe_path = os.path.join(config.anonymized_folder, f'{file_name}_desc.json')
+#Status da Task
+@api.route("/simple_task_status/<task_id>")
+class SimpleStatus(Resource):
+    def get(self, task_id):
+        stat = simple_app.AsyncResult(task_id, app=simple_app)
+        #stat = AsyncResult(task_id).state
 
-        output_size = request.args.get('sz', default = 1000, type = int)
-        epsilon = request.args.get('eps', default = 1, type = int)
-        threshold_value = request.args.get('threshold_value', default = 20, type = int)
-        degree_of_bayesian_network = request.args.get('degree', default = 2, type = int)
+        if(stat.state == 'PENDING') :
+            print("Invoking Method ")
+            return "Status of the Task " + stat.state, 200
 
-        print(f'Running w/ parametes: size={output_size} eps={epsilon} thr={threshold_value} deg={degree_of_bayesian_network}')
+        elif(stat.state == 'SUCCESS') :
+            print("Invoking Method ")
+            return "Status of the Task " + stat.state, 302
+        
+    
 
-        describer = DataDescriber(category_threshold=threshold_value)
-        describer.describe_dataset_in_correlated_attribute_mode(dataset_file=os.path.join(config.uploaded_folder, f'{file_name}.csv'), 
-                                                                epsilon=epsilon,
-                                                                k=degree_of_bayesian_network)
-        describer.save_dataset_description_to_file(describe_path)
-        generator = DataGenerator()
-        generator.generate_dataset_in_correlated_attribute_mode(output_size, describe_path) # resolve this params
-        generator.save_synthetic_data(data_path)
-
-        return send_file(data_path, as_attachment=True)
-    '''
-
-class SynthetizeCorrelatedStatus(Resource):
-    def get(self, file_id):
-
-        file = FileModel.find_file(file_id)
-
-        return file.json(), 200
-
-class SynthetizeCorrelatedDataset(Resource):
-    def get():
-        return 500
+    
+#Resultado da Task
+@api.route('/simple_task_result/<task_id>')
+class SimpleResult(Resource):
+    def get(self, task_id):
+        res = simple_app.AsyncResult(task_id)
+        
+        #return "Result of the Task " + str(res.result)
+        return send_file(res.result, as_attachment=True)
