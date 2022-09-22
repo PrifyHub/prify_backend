6a8124c4e7e5fd219d39c29e62edcaf4828883dc
unknown <aglailsonsantiago@gmail.com>
2022-09-22 13:45:09 -0300

    Implementation of the asynchronous solution with MongoDB, RabbitMQ e Celery

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
-from flask import Flask
-from flask_restful import Api
-from distutils.command.config import config
 from pathlib import Path
+
+from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
+
 import prify.config as config
-from prify.synthetics_api import SynthetizeCorrelated, SynthetizeCorrelatedDataset, SynthetizeCorrelatedStatus
+from prify import apis
 
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-api = Api(app)
-
-@app.before_first_request
-def cria_banco():
-    banco.create_all()
-
-api.add_resource(SynthetizeCorrelated, '/synthetics/correlated')
-api.add_resource(SynthetizeCorrelatedStatus, '/synthetics/correlated/status/<string:file_id>')
-api.add_resource(SynthetizeCorrelatedDataset, '/synthetics/correlated/dataset/<string:file_id>')
-
-if __name__ == '__main__':
+def init():    
     Path(config.anonymized_folder).mkdir(parents=True, exist_ok=True)
     Path(config.uploaded_folder).mkdir(parents=True, exist_ok=True)
-
+        
+if __name__ == "__main__":
+    init()
+    app = Flask(__name__)
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
-    from sql_alchemy import banco
-    banco.init_app(app)
-    
+    apis.api.init_app(app)
     app.run(debug=True)
---
+++ prify/apis/synthetics_api.py
@@ -0,0 +1,61 @@
+import os
+import uuid
+import pymongo
+import boto3
+
+import prify.config as config
+from flask import jsonify, request, send_file
+from flask_restx import Namespace, Resource
+from celery import Celery
+
+api = Namespace("synthetics")
+
+simple_app = Celery('simple_worker', broker='amqp://localhost',
+                                    backend='mongodb://localhost:27017/mydb')
+@api.route("/correlated")
+class SynthetizeCorrelated(Resource):
+    
+    def post(self):
+        
+
+        file_name = str(uuid.uuid4())
+
+        uploaded_file = request.files['file']
+
+        if uploaded_file.mimetype != 'text/csv':
+            return jsonify({'status': 'error', 'message': 'The file type must be CSV.'})
+
+        
+        api.logger.info("Invoking Method ")
+        uploaded_file.save(os.path.join(config.uploaded_folder, file_name+'.csv')) # we should delete this data after the process
+        r = simple_app.send_task('tasks.anonimyze_file', kwargs={'file_name':file_name})
+        api.logger.info(r.backend)
+        return r.id, 202
+        #return self.anonimyze_file(file_name=file_name)
+
+#Status da Task
+@api.route("/simple_task_status/<task_id>")
+class SimpleStatus(Resource):
+    def get(self, task_id):
+        stat = simple_app.AsyncResult(task_id, app=simple_app)
+        #stat = AsyncResult(task_id).state
+
+        if(stat.state == 'PENDING') :
+            print("Invoking Method ")
+            return "Status of the Task " + stat.state, 200
+
+        elif(stat.state == 'SUCCESS') :
+            print("Invoking Method ")
+            return "Status of the Task " + stat.state, 302
+        
+    
+
+    
+#Resultado da Task
+@api.route('/simple_task_result/<task_id>')
+class SimpleResult(Resource):
+    def get(self, task_id):
+        res = simple_app.AsyncResult(task_id)
+        
+        #return "Result of the Task " + str(res.result)
+        return send_file(res.result, as_attachment=True)
